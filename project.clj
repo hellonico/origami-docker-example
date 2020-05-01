e495bca4870d9564d52fdfddd3d24c9f6303f821
Nicolas Modrzyk <hellonico@gmail.com>
2020-05-01 11:15:53 +0900

    only linux

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["vendredi" "https://repository.hellonico.info/repository/hellonico/"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [origami "4.3.0-6"]]
+                 [origami "4.3.0-6" :exclusions [[opencv/opencv-native]]]
+                 [opencv/opencv-native "4.3.0-1" :classifier "linux_64"]]
   :main indexation.image-processing)
